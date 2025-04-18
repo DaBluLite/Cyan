417225d19cf8da1f6f1bda1f74709c2d13b6ccd9
DaBluLite <dablulite@gmail.com>
2025-04-19 01:59:58 +0300

    A good 20% completed (Main chat UIs, everything else is still unthemed). Is not enforced on visual-refresh, don't know how it reacts to the old UI

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const header = require('postcss-header');
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
+    map: { annotation: false },
     plugins: [
+        header({header: `/**
+            * Credits:
+            * Icons by Bootrstrap Icons
+            * Fonts by Google Fonts
+            */`}),
         require('@tailwindcss/postcss'),
     ]
 }
